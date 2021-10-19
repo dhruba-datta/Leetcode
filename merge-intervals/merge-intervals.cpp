3991893150b14efac9bdee1432d89df067bcc7a9
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-10-19 23:11:40 +0530

    Time: 52 ms (12.87%), Space: 14 MB (95.51%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& v) {
-        
-        sort(v.begin(),v.end());
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>>ans;
-   
-        ans.push_back(v[0]);
-
-        int first = ans[0][0];
-        int last = ans[0][1];
-        int k=0;
-
-        for(int i=1;i<v.size();i++)
-        {
-
-            if(v[i][0]<=last)
-            {
-                ans[k][1] = max(v[i][1],last);
-                ans[k][0] =  min(v[i][0],first);
-                first = ans[k][0];
-                last = ans[k][1];
-               // cout<<" -> "<<first<<" "<<last<<endl;
+        int n = intervals.size();
+        ans.push_back({intervals[0][0],intervals[0][1]});
+       
+        if(n==1)
+            return ans;
+        
+        int a=0, b=0, i=1;
+        while(i<n){
+            if(ans[a][1]>=intervals[i][0]){
+                ans[a][1]= max(ans[a][1],intervals[i][1]);
+                i++;
             }
             else{
-
-                vector<int>temp;
-                temp.push_back(v[i][0]);
-                temp.push_back(v[i][1]);
-
-                ans.push_back(temp);
-
-                k++;
-                first =  ans[k][0];
-                //cout<<"D";
-                last =  ans[k][1];
-
+                ans.push_back({intervals[i][0],intervals[i][1]});
+                a++;
             }
+        }
+        return ans;     
     }
-            return ans;
-    }
-
 };
-//credit goes to dhruba
