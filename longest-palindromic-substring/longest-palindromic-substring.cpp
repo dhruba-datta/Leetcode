846603940aa43a676a23066785440dae2f76d08c
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-10-22 21:07:51 +0530

    Time: 21 ms (79.80%), Space: 7 MB (92.77%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,45 +1,43 @@
-class Solution
-{
+class Solution {
 public:
-    string longestPalindrome(string s)
-    {
+    string longestPalindrome(string s) {
         int n = s.size();
-        if (n == 0)
-            return "";
-
-        // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
-        bool dp[n][n];
-
-        //Initialize with false
-
-        memset(dp, 0, sizeof(dp));
-
-        //Every Single character is palindrome
-        for (int i = 0; i < n; i++)
-            dp[i][i] = true;
-
-        string ans = "";
-        ans += s[0];
-
-        for (int i = n - 1; i >= 0; i--)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (s[i] == s[j])
-                {
-                    //If it is of two character OR if its susbtring is palindrome.
-                    if (j - i == 1 || dp[i + 1][j - 1])
-                    {
-                        //Then it will also a palindrome substring
-                        dp[i][j] = true;
-
-                        //Check for Longest Palindrome substring
-                        if (ans.size() < j - i + 1)
-                            ans = s.substr(i, j - i + 1);
-                    }
+        int st=0;
+        int l=0, r, mx=0;
+        if(n<2) return s;
+        
+        
+        for(int i=0; i<n-1; i++){
+            l=i; r=i+1;
+             while(l>=0 && r<n){
+                if(s[l]==s[r]){
+                    l--; r++;
                 }
+                else
+                    break;
+            }
+            int len= r-l-1;
+            if(len>mx){
+                mx= len;
+                st= l+1;
             }
         }
-        return ans;
+        for(int i=0; i<n-1; i++){
+            l=i; r=i;
+            while(l>=0 && r<n){
+                if(s[l]==s[r]){
+                    l--; r++;
+                }
+                else
+                    break;
+            }
+            int len= r-l-1;
+            if(len>mx){
+                mx= len;
+                st= l+1;
+            }
+        }
+        
+        return s.substr(st, mx);
     }
 };
