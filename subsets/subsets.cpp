2a189718686d17aa24edf3a5dbd9b47a601d3121
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-11-11 09:29:04 +0530

    Time: 0 ms (100.00%), Space: 15.9 MB (14.04%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -1,25 +1,22 @@
 class Solution {
-public:
-    void findSet(int idx, vector<vector<int>> &ans, vector<int>& nums){
-        if(idx==nums.size()-1){
-            ans.push_back({});
-            ans.push_back({nums[idx]});
+    void solve(vector<vector<int>> &ans, int i, vector<int>& ip, vector<int> op){
+        if(ip.size()==i){
+            ans.push_back(op);
+            return;
         }
-        else{
-            findSet(idx+1, ans, nums);
-            int n=ans.size();
-            for(int i=0; i<n; i++){
-                vector<int> temp= ans[i];
-                temp.push_back(nums[idx]);
-                ans.push_back(temp);
-            }   
-        }     
-    }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ans;
+        solve(ans, i+1, ip, op);
         
-        findSet(0, ans, nums);
+        op.push_back(ip[i]);
+        solve(ans, i+1, ip, op);
+        op.pop_back();
+        return;
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& ip) {
+        vector<vector<int>> ans;
+        vector<int>op;
+        solve(ans, 0, ip, op);
         return ans;
+        
     }
 };
