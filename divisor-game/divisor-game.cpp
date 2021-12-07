9aa5959804724c737127f534c771048be9df4c9f
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-12-07 22:43:13 +0530

    Time: 14 ms (8.81%), Space: 6 MB (31.49%) - LeetHub

--- divisor-game/divisor-game.cpp
+++ divisor-game/divisor-game.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
-    bool divisorGame(int n) {
-        if(n%2==0)
-            return true;
-        else
-            return false;
+    bool divisorGame(int N) {
+        vector<bool> dp(N+1);
+        dp[1]=false;
+        for(int i=2;i<=N;i++){
+            dp[i]=false;
+            for(int j=i-1;j>=1;j--){
+                if(i%j==0 && dp[i-j]==false){
+                     dp[i]=true;
+                }
+            }
+        }
+        return dp[N];
     }
 };
