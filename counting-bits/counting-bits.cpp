a3c72a1476d0d051a0d8754fcf0e873c0052ec00
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-12-09 11:40:13 +0530

    Time: 4 ms (88.57%), Space: 7.9 MB (46.12%) - LeetHub

--- counting-bits/counting-bits.cpp
+++ counting-bits/counting-bits.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
-        int dp[n+1];
+        vector<int> dp(n+1);
+      
         dp[0]=0;
         int k=1;
        
         for(int i=1; i<=n; i++){
-            if(k*2==i){
+            if(k*2==i)
                 k = k*2;
-                dp[i]= 1+dp[i-k];
-            }
-            else
-                dp[i]= 1+dp[i-k];
             
+            dp[i]= 1+dp[i-k];  
         }
-        /*
-        for(int i=0; i<=n; i++){
-            cout<<i<< "-- "<<dp[i]<<endl;
-        }
-        */
-        for(int i=0; i<=n; i++){
-            ans.push_back(dp[i]);
-        }
-        
-        return ans;
+       
+        return dp;
     }
 };
