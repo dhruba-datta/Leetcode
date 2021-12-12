d1880a45b2c887ecd6703a8c57a7eb923792ac98
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-12-12 18:53:16 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (81.79%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -4,7 +4,8 @@ public:
         int t[n+1];
         
         t[1]=1;
-        if(n>=2) t[2]=2;
+        if(n>=2) t[2]=2; //if we don't give the condition then it will give runtime error for i/p 1
+        
         for(int i=3; i<=n; i++)
             t[i]= t[i-1]+t[i-2];
         
