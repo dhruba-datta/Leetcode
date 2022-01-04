31160cfc62577c845f405b2255249c4904940826
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-01-04 09:44:25 +0530

    Time: 3 ms (36.91%), Space: 5.7 MB (99.97%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1)
-            return n;
-        int ans = fib(n-1)+fib(n-2);
-        return ans;
+        int ans[n+2];
+        ans[0]=0;
+        ans[1]=1;
+        
+        for(int i=2; i<=n; i++)
+            ans[i]=ans[i-1]+ans[i-2];
+        
+        return ans[n];
     }
 };
