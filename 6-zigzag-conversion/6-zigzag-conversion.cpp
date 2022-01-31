87417fa90f58b7c38a12de80ef97d01780ce14a2
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-01-31 18:18:00 +0530

    Time: 15 ms (50.57%), Space: 8.1 MB (88.90%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        string result;
+        string ans;
         if (numRows == 1) return s;
         
-        for (int row = 0; row < numRows; row++){
-            int i = row;
-            while (i < s.length()){
-                result.push_back(s[i]);
-                i += (numRows - i % (numRows - 1) - 1) * 2;
+        int step = 2 * numRows - 2;
+        
+        for(int i=0; i<numRows; i++){
+            if(i==0 || i==numRows-1){
+                for(int j=i; j<s.size(); j+=step){
+                     ans.push_back(s[j]);
+                }
+            }
+            
+            else{
+                int j = i;
+                bool flag = true;
+                int step1 = 2 * (numRows - 1 - i);
+                int step2 = step - step1;
+
+                while (j < s.length()) {
+                     ans.push_back(s[j]);
+                    if (flag)
+                        j = j + step1;
+                    else
+                        j = j + step2;
+                    flag = !flag;
+                }
             }
         }
-        return result;
+        return ans;
     }
 };
