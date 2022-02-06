e7ddf0f5513c5f6f2b1fee9de9b13a9bf887dbb2
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-02-06 15:16:44 +0530

    Time: 16 ms (68.39%), Space: 8.3 MB (71.58%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -7,11 +7,12 @@ public:
             mp[x] = -1;
         
         int i=0, start=-1, ans=0;
-        for(int i=0; i<n; i++){
+        while(i<n){
             if(mp[s[i]] > start)
                 start = mp[s[i]];
             mp[s[i]] = i;
             ans = max(ans, i-start);
+            i++;
         }
         return ans;
     }
