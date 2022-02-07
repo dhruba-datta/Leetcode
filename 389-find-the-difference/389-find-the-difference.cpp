374d5d72972c0b2095e0098d439ea0a647a5af2c
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-02-07 13:24:09 +0530

    Time: 3 ms (78.44%), Space: 6.6 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> v(26, 0);
-        int n = s.size(), i=0;
-        char ans;
-        
-        for(auto x:t){
-            v[x-'a'] += 1;
-        }
-        for(auto x:s){
-            v[x-'a'] -= 1;
-        }
-        
-        for(int i=0; i<26; i++){
-            if(v[i] != 0)
-                ans = i + 'a';
-        }
-        
-        return ans;
+      sort(s.begin(),s.end());
+      sort(t.begin(),t.end());
+      for( int i =0;i<s.size();i++){
+        if(s[i]!=t[i])return t[i];
+      }
+      return t[s.size()];        
     }
 };
