dde6d4e86abdd942195f6c275581e8f8c5b8447e
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-02-08 09:21:07 +0530

    Time: 4 ms (43.34%), Space: 5.9 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while(num>9){
-            int temp = num%10;
-            num = num/10 + temp;
-        }
-        return num;
+        if(num == 0) return 0;
+        else if(num%9 == 0) return 9;
+        else return num%9;
     }
 };
