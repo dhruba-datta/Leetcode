8a8b581089cc2beb6c1c2c1a0335a15361ac1149
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-02-11 12:01:26 +0530

    Time: 14 ms (44.26%), Space: 7.4 MB (25.75%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -3,13 +3,16 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.size(), m = s2.size(), j=0;
         vector<int> a(26, 0), b(26, 0);
-        if(n<m){
+        
+        if(n>m)
+            return false;
+        
+        else if(n<m){
             for(auto x:s1)
                 a[x-'a']++;
-
-            for(int i=0; i<n; i++){
+            for(int i=0; i<n; i++)
                 b[s2[i]-'a']++;
-            }
+            
             if(a == b) return true;
             
             for(int i=n; i<m; i++){
@@ -17,26 +20,9 @@ public:
                 b[s2[j]-'a']--;
                 if(a==b) return true;
                 j++;
-            }
-            
+            }         
         }
-        else if(n>m){
-            for(auto x:s2)
-                b[x-'a']++;
-
-            for(int i=0; i<n; i++){
-                a[s1[i]-'a']++;
-            }
-            if(a == b) return true;
-            
-            for(int i=n; i<m; i++){
-                a[s1[i]-'a']++;
-                a[s1[j]-'a']--;
-                if(a==b) return true;
-                j++;
-            }
-            
-        }
+        
         else{
             for(auto x:s1)
                 a[x-'a']++;
