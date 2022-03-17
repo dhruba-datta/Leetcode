0b84b1971702c817398ee74ebaa9ade38c7f85ac
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-03-17 23:53:40 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (83.25%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        int n = s.size();
-        int count = 0, ans = 0;
         stack<int> st;
+        int count = 0;
         
         for(auto x:s){
             if(x == '('){
-                st.push(-1);
-            }
-            else if(x == ')' && st.top() == -1){
-                st.pop();
-                st.push(1);
-            }
-            else if(x == ')' && st.top() != -1){
-                while(st.top() != -1){
-                    count += st.top();
-                    st.pop();
-                }
-                st.pop();
-                st.push(2*count);
+                st.push(count);
                 count = 0;
             }
+            else{
+                count = st.top() + max(1, 2*count);
+                st.pop();
+            }
         }
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-        }
-        return ans;
+        return count;
     }
 };
