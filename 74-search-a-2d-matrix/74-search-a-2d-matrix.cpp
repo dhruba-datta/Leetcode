ba68b66de8183291a076a1d58d49c9fd1023e2af
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-03-30 11:48:26 +0530

    Time: 9 ms (23.12%), Space: 9.6 MB (52.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>> &m, int t) {
-        // pointless edge case we still have to consider
-		if (!m.size() || !m[0].size()) return false;
-        // support variables
-		int row, l = 0, r = m.size() - 1, mid;
-        while (l < r) {
-            mid = (l + r) / 2;
-            // moving l up if needed
-            if (m[mid].back() < t) l = mid + 1;
-            // moving r down if needed
-            else if (m[mid][0] > t) r = mid - 1;
-            // we found our row!
-            else {
-                l = mid;
-                break;
-            };
-        }
-        // storing the value of the new found row
-        row = l;
-        // resetting l and r to run a binary search on the rows
-        l = 0;
-        r = m[0].size() - 1;
-        while (l <= r) {
-            mid = (l + r) / 2;
-            // moving l up if needed
-            if (m[row][mid] < t) l = mid + 1;
-            // moving r down if needed
-            else if (m[row][mid] > t) r = mid - 1;
-            // we found our value!
-            else return true;
+    bool searchMatrix(vector<vector<int>>& mt, int target) {
+        int n = mt.size(), m = mt[0].size();
+        int low = 0, high = m*n-1;
+        
+        while(low <= high){
+            int mid = low+(high-low) / 2;
+            int row = mid/m;
+            int col = mid%m;
+            
+            if(mt[row][col] == target) return true;
+            
+            else if(mt[row][col] >= target) high = mid-1;
+            
+            else low = mid+1;
         }
         return false;
     }
