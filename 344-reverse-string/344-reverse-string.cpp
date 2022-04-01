5db9ab4ad487fbd3bbc8be9fa37efe70ced76ac2
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-04-01 09:00:30 +0530

    Time: 24 ms (76.05%), Space: 23.1 MB (76.65%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
-        int low = 0, high = n-1;
-        
-        while(low <= high){
-            swap(s[low], s[high]);
-            low++; high--;
-        }
+        reverse(s.begin(), s.end());
         return;
     }
 };
