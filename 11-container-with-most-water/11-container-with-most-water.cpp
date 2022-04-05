6a9fbfc0468e83a5850e1bcb4b608ae6c84a0c71
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-04-05 10:42:47 +0530

    Time: 97 ms (78.76%), Space: 59 MB (81.47%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -6,8 +6,8 @@ public:
         int start=0, end=n-1;
         
         while(start<end){
-            int a= min(height[start], height[end]) * (end-start);
-            ans = max(ans, a);
+            int area= min(height[start], height[end]) * (end-start);
+            ans = max(ans, area);
             
             if(height[start] > height[end])
                 end--;
