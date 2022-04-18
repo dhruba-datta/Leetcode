43c5c84ceee56669e05525dcd8e82929e83dd40f
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-04-18 12:52:05 +0530

    Time: 6 ms (21.04%), Space: 7.7 MB (63.55%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -11,27 +11,20 @@
  */
 class Solution {
 public:
+    TreeNode* ans;
     TreeNode* increasingBST(TreeNode* root) {
-        vector<int> a(1001, 0);
-        find(root, a);
-        
-        struct TreeNode *ans = new TreeNode(-1);
-        struct TreeNode *temp = ans;
-
-        
-        for(int i=0; i<1001; i++){
-            if(a[i]==1){
-                ans->right = new TreeNode(i);
-                ans = ans->right;
-            }
-        }
+        TreeNode* temp = new TreeNode(0);
         ans = temp;
-
-        return ans->right;
+        inorder(root);
+        return temp->right;
     }
-    void find(TreeNode* root,  vector<int> &a){
-        a[root->val] = 1;
-        if(root->left != NULL) find(root->left, a);
-        if(root->right != NULL) find(root->right, a);
+    
+    void inorder(TreeNode* root){
+        if(!root) return;
+        inorder(root->left);
+        root->left = NULL;
+        ans->right = root;
+        ans = ans->right;
+        inorder(root->right);
     }
 };
