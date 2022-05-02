05bb2eb826e6ef3ab26c146314ab80884490c5c2
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-05-02 12:17:33 +0530

    Time: 11 ms (74.52%), Space: 16.2 MB (57.28%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -4,9 +4,7 @@ public:
         int x=0;
         
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] % 2 == 0 && i == 0) x++;
-            
-            else if(nums[i] % 2 == 0 && i != 0){
+            if(nums[i] % 2 == 0){
                 swap(nums[i], nums[x]);
                 x++;
             }
