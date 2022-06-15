347ac712c0da2e144ce01022baaf8ac2f83184c0
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-06-15 10:17:16 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.64%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        double ans = 1.0;
+        long long m = n;
+        
+        if(m<0) m *= -1;
+        
+        while(m){
+            if(m % 2){
+                ans *= x;
+                m--;
+            }
+            else{
+                x *= x;
+                m /= 2;
+            }
+        }
+        
+        return (n<0) ? 1.0/ans : ans;
     }
 };
