f84a377fb9581e4e230230d51a1ce5e83ab3f920
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-08-04 20:41:54 +0530

    Time: 42 ms (44.21%), Space: 16.1 MB (60.35%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -11,20 +11,16 @@
  */
 class Solution {
 public:
-    long long count = 0;
     long long pathSum(TreeNode* root, long long sum) {
-        if(root){
-            help(root, sum);
-            pathSum(root->left, sum);
-            pathSum(root->right, sum);
-        }
-        return count;
+        if(!root) return 0;
+        return pathSum(root->left, sum) + pathSum(root->right, sum) + help(root, sum);
     }
-    void help(TreeNode* root, long long sum){
-        if(!root) return;
+    long long help(TreeNode* root, long long sum){
+        if(!root) return 0;
+        long long count = 0;
         if(root->val == sum) count++;
-        help(root->left, sum-root->val);
-        help(root->right, sum-root->val);
-        return;
+        count += help(root->left, sum-root->val);
+        count += help(root->right, sum-root->val);
+        return count;
     }
 };
