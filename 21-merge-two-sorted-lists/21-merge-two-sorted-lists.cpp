1e7d40320b80a44634c2c6d3a92ed99a762ea078
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-08-09 03:12:27 +0530

    Time: 12 ms (56.73%), Space: 14.7 MB (81.48%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -14,13 +14,18 @@ public:
         if(!l1) return l2;
         if(!l2) return l1;
         
-        if(l1->val < l2->val){
-			l1->next = mergeTwoLists(l1->next, l2);
-			return l1;
-		}
-		else{
-			l2->next = mergeTwoLists(l1, l2->next);
-			return l2;            
-		}
+        if(l1->val > l2->val) swap(l1, l2);
+        ListNode* ans = l1;
+        
+        while(l1 && l2){
+            ListNode* temp = NULL;
+            while(l1 && l1->val <= l2->val){
+                temp = l1;
+                l1 = l1->next;
+            }
+            temp->next = l2;
+            swap(l1, l2);
+        }
+        return ans;
     }
 };
