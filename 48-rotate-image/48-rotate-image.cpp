900e8c9d8414b8c961144e8d4738f47a3c651ffd
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-09-02 03:31:11 +0530

    Time: 8 ms (8.97%), Space: 7.2 MB (33.49%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n= matrix.size();
+        int n=matrix.size();
         
         for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
@@ -9,8 +9,7 @@ public:
             }
         }
         
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++)
             reverse(matrix[i].begin(), matrix[i].end());
-        }
     }
 };
