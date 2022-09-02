1e20545923ea3d0e248da1a931bfd8395749de3f
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-09-02 19:02:56 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.92%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n));
-        return dfs(dp, 0, 0);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return help(0,0,m,n,dp);
     }
-    int dfs(vector<vector<int>>& dp, int i, int j) {
-        if(i >= size(dp)   || j >= size(dp[0]))   return 0;     // out of bounds - invalid
-        if(i == size(dp)-1 && j == size(dp[0])-1) return 1;     // reached end - valid path
-        if(dp[i][j]) return dp[i][j];                           // directly return if already calculated
-        return dp[i][j] = dfs(dp, i+1, j) + dfs(dp, i, j+1);    // store the result in dp[i][j] and then return
+    
+    int help(int start, int end, int m, int n, vector<vector<int>> &dp){
+        if(start > m || end > n) return 0;
+        if(start == m-1 && end == n-1) return 1;
+        
+        if(dp[start][end] != -1) return dp[start][end];
+        return dp[start][end] = help(start+1, end, m, n, dp) + help(start, end+1, m, n, dp);
     }
 };
