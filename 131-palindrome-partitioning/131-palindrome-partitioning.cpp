bb226d32654f030be590c00b0cddf2fa65637412
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-09-07 20:58:28 +0530

    Time: 215 ms (51.54%), Space: 84.1 MB (28.58%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
+    vector<vector<string>> ans;
+    vector<string> temp;
+    
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> temp;
-        help(0, s, temp, ans);
+        help(0, s);
         return ans;
     }
     
+    void help(int idx, string s){
+        if(idx == s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i=idx; i<s.size(); i++){
+            if(pal(s, idx, i)){
+                temp.push_back(s.substr(idx, i-idx+1));
+                help(i+1, s);
+                temp.pop_back();
+            }
+        }
+    }
+    
+    bool pal(string s, int start, int end){
+        while(start <= end) if(s[start++] != s[end--]) return false;
+        return true;
+    }
+};
+/*
     void help(int index, string s, vector<string> &temp, vector<vector<string>> &ans){
         if(index == s.size()){
             ans.push_back(temp);
@@ -28,4 +50,4 @@ public:
         }
         return true;
     }
-};
+*/
