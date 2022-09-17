ed274389d795aad51b14bb6411ae91e902cc12aa
Dhruba Datta <dhrubadattaanjan@gmail.com>
2022-09-17 07:41:19 +0530

    Time: 137 ms (5.62%), Space: 45.7 MB (8.96%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,40 +1,57 @@
 class Solution {
 public:
-    void solve(int col, vector<vector<string>> &ans, vector<string> &board, vector<int> &left, vector<int> &upperD, vector<int> &lowerD, int n){
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string temp(n, '.');
+        for(int i=0; i<n; i++)
+            board[i] = temp;
+        help(0, n, ans, board);
+        return ans;
+    }
+    
+    void help(int col, int n, vector<vector<string>> &ans, vector<string> &board){
         if(col == n){
             ans.push_back(board);
             return;
         }
         
         for(int row=0; row<n; row++){
-            if(left[row]==0 && lowerD[row+col]==0 && upperD[n-1+col-row]==0){
+            if(safe(row, col, board, n)){
                 board[row][col] = 'Q';
-                left[row] = 1;
-                lowerD[row+col] = 1;
-                upperD[n-1+col-row] = 1;
-                
-                solve(col+1, ans, board, left, upperD, lowerD, n);
-                
+                help(col+1, n, ans, board);
                 board[row][col] = '.';
-                left[row] = 0;
-                lowerD[row+col] = 0;
-                upperD[n-1+col-row] = 0;
             }
         }
     }
     
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n);
-        string s(n, '.');
-        
-        for(int i=0; i<n; i++){
-            board[i] = s;
-        }
-        
-        vector<int> left(n,0), upperD(2*n-1,0), lowerD(2*n-1,0);
-        solve(0, ans, board, left, upperD, lowerD, n);
-        
-        return ans;
+    bool safe(int row, int col, vector<string> board, int n){
+         int duprow = row;
+         int dupcol = col;
+
+         while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q')
+              return false;
+            row--;
+            col--;
+          }
+
+          col = dupcol;
+          row = duprow;
+          while (col >= 0) {
+            if (board[row][col] == 'Q')
+              return false;
+            col--;
+          }
+
+          row = duprow;
+          col = dupcol;
+          while (row < n && col >= 0) {
+            if (board[row][col] == 'Q')
+              return false;
+            row++;
+            col--;
+          }
+          return true;
     }
 };
